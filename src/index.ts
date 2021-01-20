008a9c17ed5e83e47a7667ceb68f18c1889233d9
Kamil Neczaj <kneczaj@gmail.com>
2021-01-20 23:24:27 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,48 @@
 // HTTP forward proxy server that can also proxy HTTPS requests
 // using the CONNECT method
-
 // requires https://github.com/nodejitsu/node-http-proxy
 
-import * as httpProxy from 'http-proxy';
 import * as http from "http";
-import * as url from "url";
 import * as net from "net";
+import * as express from 'express';
+import * as proxy from 'express-http-proxy';
 
 process.on('uncaughtException', logError);
 
 function truncate(str) {
-	var maxLength = 64;
+	const maxLength = 64;
 	return (str.length >= maxLength ? str.substring(0,maxLength) + '...' : str);
 }
 
 function logRequest(req) {
 	console.log(req.method + ' ' + truncate(req.url));
-	for (var i in req.headers)
-		console.log(' * ' + i + ': ' + truncate(req.headers[i]));
+	req.headers.forEach((header, i) =>
+		console.log(' * ' + i + ': ' + truncate(req.headers[i]))
+	);
 }
 
 function logError(e) {
 	console.warn('*** ' + e);
 }
 
-// this proxy will handle regular HTTP requests
-var regularProxy = new httpProxy.createProxyServer({});
+const app = express();
+app.use('/', (req, res, next) => {
+	logRequest(req);
+	proxy(req.url)(req, res, next);
+})
 
 // standard HTTP server that will pass requests
 // to the proxy
-var server = http.createServer(function (req, res) {
-  logRequest(req);
-  const uri = url.parse(req.url);
-  regularProxy.proxyRequest(req, res, {
-  	host: uri.hostname,
-  	port: uri.port || 80
-  });
-});
+const server = http.createServer(app);
 
-// when a CONNECT request comes in, the 'upgrade'
+// when a CONNECT request comes in, the 'connect'
 // event is emitted
-server.on('upgrade', function(req, socket, head) {
+server.on('connect', function(req, socket, head) {
 	logRequest(req);
 	// URL is in the form 'hostname:port'
-	var parts = req.url.split(':', 2);
+	const parts = req.url.split(':', 2);
 	// open a TCP connection to the remote host
-	var conn = net.connect(parts[1], parts[0], function() {
+	const conn = net.connect(parts[1], parts[0], function() {
 		// respond to the client that the connection was made
 		socket.write("HTTP/1.1 200 OK\r\n\r\n");
 		// create a tunnel between the two hosts
@@ -55,4 +51,4 @@ server.on('upgrade', function(req, socket, head) {
 	});
 });
 
-server.listen(3333);
+server.listen(process.env['PORT'] || 3333);
